641a31d63d79d2a5f0374e4b63852637be8b8955
fuka-takashima <fuka.takashima@dena.com>
2023-07-20 17:55:53 +0900

    Initial Commit

---
+++ MyCoreData/MyCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataApp.swift
+//  MyCoreData
+//
+//  Created by Fuka Takashima on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
